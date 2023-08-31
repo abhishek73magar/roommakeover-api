968cd7a20ce69a632fccb40475cb627945452eed
Coder73-avi <aavishek60@gmail.com>
2023-09-01 00:33:29 +0545

    add secure true

--- libs/setCookie.js
+++ libs/setCookie.js
@@ -1,7 +1,7 @@
 exports.setCookie = (res, name, value, expireDate) => {
   return res.cookie(name, value, {
     httpOnly: true,
-    secure: false,
+    secure: true,
     path: "/",
     sameSite: "none",
     maxAge: expireDate,
