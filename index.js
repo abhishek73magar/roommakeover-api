fa25a23004f2c49254c8b7cd479f4a3f36da348e
abhishek <Coder73-avi>
2024-02-27 20:22:00 +0545

    update

--- index.js
+++ index.js
@@ -15,10 +15,9 @@ const port = process.env.PORT;
 // console.log(corsList.split(","));
 
 app.use(cookieParser());
-app.use("/api/image", express.static("public"));
 app.use(express.json());
 
-
+app.use("/api/image", express.static("public"));
 app.use(cors({ origin: origins, credentials: true }));
 // app.use(auth);
 app.use((req, res, next) => { console.log(req.method, "-", req.url, "-", new Date().toISOString()); return next()})
