5504cd8a86b5c03ad3ed8d13a2797ee7d38a360b
abhishek <Coder73-avi>
2024-04-04 19:55:30 +0545

    display blog only which is published

--- model/blogModel.js
+++ model/blogModel.js
@@ -3,13 +3,14 @@ const knex = require("../db");
 exports.getBlogModel = async(query)  => {
   try {
     const result = { data: [], total_page: 1 }
-    let res = knex('blogs')
+    let status = '1'
+    let res = knex('blogs').where({ status })
     if(query.hasOwnProperty('limit')) {
       let limit = query.limit || 5
       let offset = 0
       if(query.hasOwnProperty('pagenumber')) { offset = (query.pagenumber *  limit) - limit }
       
-      const [blogs] = await knex('blogs').count();
+      const [blogs] = await knex('blogs').where({ status }).count();
       result["total_page"] = Math.ceil(blogs.count / limit)
       res = res.limit(limit).offset(offset)
     } 
@@ -23,9 +24,9 @@ exports.getBlogModel = async(query)  => {
 
 exports.getBlogByTitleModel = async(title) => {
   try {
-    console.log(title)
+    // console.log(title)
     title = title.toLowerCase().replace(/-/g, ' ').replace(/\$/g, '?')
-    let query = `SELECT * FROM blogs WHERE lower(title)=?`
+    let query = `SELECT * FROM blogs WHERE lower(title)=? AND status='1'`
     const { rows } = await knex.raw(query, [title])
     if(rows.length === 0) throw "Blog not found !"
     return rows[0]
@@ -38,7 +39,7 @@ exports.getBlogByTitleModel = async(title) => {
 exports.getBlogOtherInfoModel = async(query) => {
   try {
     if(query.status === 'title'){
-      const blogs = await knex('blogs').select("title")
+      const blogs = await knex('blogs').where({ status: '1' }).select("title")
       return blogs
     }
     return null;
